e8ff670b03481fd943fd47f6d2652b59ffab7bcc
unknown <nmarg18@gmail.com>
2023-01-02 21:25:04 -0800

    blocks4

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let canvas, ctx;
 var levelData = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                  [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
@@ -8,11 +7,15 @@ var levelData = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                  [1, 0, 0, 1, 1, 1, 0, 0, 0, 1],
                  [1, 1, 0, 0, 1, 0, 0, 1, 1, 1],
                  [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
-                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
 function init() {
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext("2d");
-    for (var i = 0; i < 10; i++) {
+
+    ctx.beginPath();
+    ctx.fillRect(125, 35, 50, 50);
+
+    /*for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
             if (levelData[i][j] == 0) {
                 ctx.beginPath()
@@ -20,5 +23,5 @@ function init() {
                 ctx.fillrect(i * 20, j * 20, 20, 20);
             }
         }
-    }
+    }*/
 }

07d868b3c2597e8d3f6a7acc0979d17d44b4157d
unknown <nmarg18@gmail.com>
2023-01-02 21:45:03 -0800

    blocks8

--- script.js
+++ script.js
@@ -8,12 +8,14 @@
                  [1, 1, 0, 0, 1, 0, 0, 1, 1, 1],
                  [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
                  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];*/
-function init() {
-    var canvas = document.getElementById("gameCanvas");
-    var ctx = canvas.getContext("2d");
+function draw() {
+    const canvas = document.getElementById("gameCanvas");
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
 
-    ctx.beginPath();
-    ctx.fillRect(125, 35, 50, 50);
+        ctx.fillRect(25, 25, 100, 100);
+        ctx.clearRect(45, 45, 60, 60);
+        ctx.strokeRect(50, 50, 50, 50);
 
     /*for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
@@ -24,4 +26,5 @@ function init() {
             }
         }
     }*/
+    }
 }

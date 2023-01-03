da063a9fcc8c8907c756dd1614ccb3cfb091c8b9
unknown <nmarg18@gmail.com>
2023-01-02 21:11:39 -0800

    blocks3

--- script.js
+++ script.js
@@ -12,13 +12,10 @@ var levelData = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 function init() {
     canvas = document.getElementById("gameCanvas");
     ctx = canvas.getContext("2d");
-    level = new drawLevel();
-}
-
-function drawLevel() {
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
             if (levelData[i][j] == 0) {
+                ctx.beginPath()
                 ctx.fillStyle = "#FFFFFF";
                 ctx.fillrect(i * 20, j * 20, 20, 20);
             }

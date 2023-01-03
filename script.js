adf0b00dea16db694b86da243e310b1b2fd887e0
unknown <nmarg18@gmail.com>
2023-01-02 20:49:31 -0800

    rectangles

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+var levelData = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+                 [1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                 [1, 0, 0, 1, 1, 1, 0, 0, 0, 1],
+                 [1, 1, 0, 0, 1, 0, 0, 1, 1, 1],
+                 [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 function init() {
     canvas = document.getElementById("gameCanvas")
     ctx = canvas.getContext("2d")
+    drawLevel();
+}
+
+function drawLevel() {
+    for (var i; i < 10; i++) {
+        for (var j; j < 10; j++) {
+            if (levelData[i][j] == 0) {
+                ctx.fillStyle = "#FFFFFF";
+                ctx.fillrect(i * 20, j * 20, 20, 20);
+            }
+        }
+    }
 }

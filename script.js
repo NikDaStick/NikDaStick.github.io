ceec5add8475a545734bf39a67f1ffff526df11a
unknown <nmarg18@gmail.com>
2023-01-02 22:17:24 -0800

    test

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
                  [1, 0, 0, 1, 1, 1, 0, 0, 0, 1],
                  [1, 1, 0, 0, 1, 0, 0, 1, 1, 1],
                  [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
-                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];*/
+                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]];
 function draw() {
     let canvas = document.getElementById("gameCanvas");
     let ctx = canvas.getContext("2d");
@@ -15,7 +15,7 @@ function draw() {
     ctx.fillStyle = "red";
     ctx.fillRect(10, 10, 100, 50);
 
-    /*for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
             if (levelData[i][j] == 0) {
                 ctx.beginPath()
@@ -23,5 +23,5 @@ function draw() {
                 ctx.fillrect(i * 20, j * 20, 20, 20);
             }
         }
-    }*/
-}
+    }
+}*/
